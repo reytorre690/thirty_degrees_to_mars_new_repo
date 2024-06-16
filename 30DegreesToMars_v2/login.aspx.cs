13343fc773c28fdc7ba24f2acfc7e231ed19b3e6
reytorre690 <torrecamporen@gmail.com>
2024-06-15 19:42:27 -0700

    Appended Connection String, Change Query to proper tables and other changes in login aspx file (redirection)

--- 30DegreesToMars_v2/login.aspx.cs
+++ 30DegreesToMars_v2/login.aspx.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Diagnostics;
 
 namespace _30DegreesToMars_v2
 {
@@ -14,29 +15,198 @@ namespace _30DegreesToMars_v2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
         }
-        protected void Button2_Click(object sender, EventArgs e)
+
+        public void SignUp(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString);
-            con.Open();
-            SqlCommand cmd = new SqlCommand("exec dbo.authenticate_user_credentials @uname, @upass", con);
-            cmd.Parameters.AddWithValue("@uname", TextBox1.Text);
-            cmd.Parameters.AddWithValue("@upass", TextBox2.Text);
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            da.Fill(dt);
-            if (dt.Rows.Count > 0)
+
+            RequiredFieldValidator1.Enabled = true;
+            RequiredFieldValidator2.Enabled = true;
+            RequiredFieldValidator3.Enabled = true;
+            RequiredFieldValidator4.Enabled = true;
+            RequiredFieldValidator5.Enabled = true;
+            RequiredFieldValidator6.Enabled = true;
+            RequiredFieldValidator7.Enabled = false;
+            RequiredFieldValidator8.Enabled = false;
+
+            if (!Page.IsValid)
+            {
+                return;
+            }
+            string userSUid = userSU.Text;
+            string finame = fname.Text;
+            string laname = lname.Text;
+            string dateob = dob.Text;
+            string pwd = passSU.Text;
+            string rpwd = rpassSU.Text;
+
+            bool does_userid_exist = Check_If_UserID_Exist(userSUid);
+
+            if (does_userid_exist)
+            {
+
+                Label2.Visible = false;
+                ClientScript.RegisterStartupScript(this.GetType(), "does_userid_exist", "alert('Invalid username. Please use a different one.');", true);
+            }
+
+            else
+            {
+                Insert(finame, laname, dateob, userSUid, pwd, rpwd);
+                Response.Redirect("~/login.aspx");
+            }
+        }
+
+        public bool Check_If_UserID_Exist(string userSUid)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["30DegreesToMars"].ConnectionString;
+            bool does_userid_exist = false;
+            int count = 0;
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        con.Open();
+                        cmd.Connection = con;
+                        cmd.CommandText =
+                            "SELECT COUNT (*) AS count " +
+                            "FROM [project_MIS332].[dbo].[users] " +
+                            "WHERE [userid] = @User_Name_SUparameter";
+
+                        cmd.Parameters.AddWithValue("@User_Name_SUparameter", userSUid);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                count = reader.GetInt32(0);
+                            }
+                        }
+                        cmd.Dispose();
+                        con.Close();
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception.ToString());
+            }
+            if (count > 0)
+            {
+                does_userid_exist = true;
+            }
+            else
+            {
+                does_userid_exist = false;
+            }
+            return does_userid_exist;
+        }
+
+        protected void Insert(string finame, string laname, string dateob, string userSUid, string pwd, string rpwd)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["30DegreesToMars"].ConnectionString;
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        con.Open();
+                        cmd.Connection = con;
+                        cmd.CommandText =
+                            "INSERT INTO[project_MIS332].[dbo].[users] ([fname], [lname], [dob], [userid], [password],[repeatpassword], [registeredon])" +
+                            "VALUES " +
+                            "(@First_Name, @Last_Name, @Date_of_Birth, @User_Name_SU, @Password_SU,@Repeat_Password, GETDATE())";
+
+                        cmd.Parameters.AddWithValue("@First_Name", finame);
+                        cmd.Parameters.AddWithValue("@Last_Name", laname);
+                        cmd.Parameters.AddWithValue("@Date_of_Birth", dateob);
+                        cmd.Parameters.AddWithValue("@User_Name_SU", userSUid);
+                        cmd.Parameters.AddWithValue("@Password_SU", pwd);
+                        cmd.Parameters.AddWithValue("@Repeat_Password", rpwd);
+                        cmd.ExecuteNonQuery();
+                        cmd.Dispose();
+                        con.Close();
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception.ToString());
+            }
+        }
+        public void SignIn(object sender, EventArgs e)
+        {
+
+            RequiredFieldValidator1.Enabled = false; // Change the IDs as per your actual control IDs
+            RequiredFieldValidator2.Enabled = false;
+            RequiredFieldValidator3.Enabled = false;
+            RequiredFieldValidator4.Enabled = false;
+            RequiredFieldValidator5.Enabled = false;
+            RequiredFieldValidator6.Enabled = false;
+            RequiredFieldValidator7.Enabled = true;
+            RequiredFieldValidator8.Enabled = true;
+
+            string usernameSI = userSI.Text;
+            string passwordSI = passSI.Text;
+            bool does_userid_pwd_exist = Check_If_UserID_pwd_Exist(usernameSI, passwordSI);
+
+            if (does_userid_pwd_exist)
             {
                 Response.Redirect("~/dashboard.aspx");
             }
             else
             {
+                Label2.Visible = true;
 
-                Label3.Visible = true;
-                Label3.Text = "Wrong Details";
+                ClientScript.RegisterStartupScript(this.GetType(), "Hidelable", "<script type=\"text/javascript\">setTimeout(\"document.getElementById('" + Label2.ClientID + "').style.display='none'\",120000)</script>");
             }
+        }
 
+        public bool Check_If_UserID_pwd_Exist(string usernameSI, string passwordSI)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["30DegreesToMars"].ConnectionString;
+            bool SIisValid = false;
+            int countSI = 0;
+            try
+            {
+                using (SqlConnection con1 = new SqlConnection(connectionString))
+                {
+                    con1.Open();
+                    using (SqlCommand cmd1 = new SqlCommand("SELECT COUNT (*) AS count " +
+                        "FROM [project_MIS332].[dbo].[users] " +
+                        "WHERE [userid] = @User_Name_SUparameter AND [password] = @Password_SUparameter", con1))
+                    {
+                        cmd1.Parameters.AddWithValue("@User_Name_SUparameter", usernameSI);
+                        cmd1.Parameters.AddWithValue("@Password_SUparameter", passwordSI);
+
+                        using (SqlDataReader reader = cmd1.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                countSI = reader.GetInt32(0);
+                            }
+                        }
+                        cmd1.Dispose();
+                        con1.Close();
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception.ToString());
+            }
+            if (countSI > 0)
+            {
+                SIisValid = true;
+            }
+            else
+            {
+                SIisValid = false;
+            }
+            return SIisValid;
         }
     }
 }
